b15da94a1ff5f947efb292c3a1ac199d5776347c
Gopi-maker <gopibollepalli6@gmail.com>
2023-05-25 15:20:00 +0530

    storage

---
+++ main.js
@@ -0,0 +1,16 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+   localStorage.setItem('name', nameInput.value);
+  localStorage.setItem('email', emailInput.value);
+
+}
