d7bf1a5d593e69cc0477eb703dc979d257e7325c
Gopi-maker <gopibollepalli6@gmail.com>
2023-05-27 14:29:01 +0530

    deleteLocalstorage

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const userList = document.querySelector('#users');
 
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
+userList.addEventListener('click',removeItem);
 
 function onSubmit(e) {
   e.preventDefault();
@@ -22,4 +23,25 @@ function onSubmit(e) {
    //localStorage.setItem('name', nameInput.value);
   //localStorage.setItem('email', emailInput.value);
 
+
+  var li =document.createElement('li');
+  li.className='item';
+  li.appendChild(document.createTextNode(serialized));
+  userList.appendChild(li);
+
+  var dlt=document.createElement('button');
+  dlt.className="delet";
+  dlt.appendChild(document.createTextNode('Delete'));
+  li.appendChild(dlt);
+}
+
+function removeItem(e){
+  e.preventDefault();
+  if(e.target.classList.contains('delet')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      userList.removeChild(li);
+      localStorage.removeItem(nameInput.value);
+    }
+  }
 }
